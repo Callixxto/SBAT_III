b4adc731dd75267887b1636d93bdb559bffe24a1
Sandra <san.stolarek@gmail.com>
2026-06-25 15:53:41 +0200

    Connect frontend to Azure Function

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-function calculateSavings() {
+async function calculateSavings() {
     const income = Number(document.getElementById("income").value);
     const expenses = Number(document.getElementById("expenses").value);
 
-    const savings = income - expenses;
+    try {
+        const response = await fetch(
+            "https://sbat-iii-api-b5gwcsdbgcfbaycc.spaincentral-01.azurewebsites.net/api/calculateSavings",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    income: income,
+                    expenses: expenses
+                })
+            }
+        );
 
-    document.getElementById("result").innerText = "Monthly savings: " + savings;
+        if (!response.ok) {
+            throw new Error("Failed to connect to Azure Function.");
+        }
+
+        const result = await response.json();
+
+        document.getElementById("result").innerText =
+            "Monthly savings: " + result.savings;
+
+    } catch (error) {
+        console.error(error);
+        document.getElementById("result").innerText =
+            "Error calculating savings.";
+    }
 }
